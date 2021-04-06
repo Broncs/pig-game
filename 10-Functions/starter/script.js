53c30a2c421f595f02c195605a02cb4b2718ee23
broncs <jhowfrombrazil@gmail.com>
2021-04-06 19:12:42 -0300

    value vs reference in paramets

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,61 @@
 'use strict';
+/*
+const bookings = [];
+
+const createBooking = (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) => {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+console.log(createBooking('LH123', undefined, 1000));
+*/
+
+/*
+const flight = 'LH234';
+const jonas = {
+  firstName: 'jonas mendes',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.firstName = 'Mr. ' + passenger.firstName;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+// checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+const testArr = [1, 2, 3];
+
+const arrTesting = arr => {
+  arr.pop(); // this will affect the arr itself ,
+};
+
+arrTesting(testArr);
+console.log(testArr);
+*/
