b8e8e5c9bc4ae323327b69ab4d9be94249f35bbc
broncs <jhowfrombrazil@gmail.com>
2021-04-06 10:48:52 -0300

    challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -47,35 +47,35 @@ const restaurant = {
   },
 };
 
-const question = new Map([
-  ['question', 'What is the best programming language in the world ?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'Javascript'],
-  ['correct', 3],
-  [true, 'Correct 🎉'],
-  [false, 'Try again!'],
-]);
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world ?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'Javascript'],
+//   ['correct', 3],
+//   [true, 'Correct 🎉'],
+//   [false, 'Try again!'],
+// ]);
 
-console.log(question);
+// console.log(question);
 // convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
 
-console.log(question.get('question'));
-for (const [key, value] of question) {
-  if (typeof key === 'number') console.log(`Answer ${key} : ${value}`);
-}
-const answer = 3;
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key} : ${value}`);
+// }
+// const answer = 3;
 
-console.log(answer);
-console.log(question.get(question.get('correct') === answer));
+// console.log(answer);
+// console.log(question.get(question.get('correct') === answer));
 
 //convert map to array
-console.log([...question]);
-console.log(question.entries());
-console.log([...question.keys()]);
-console.log([...question.values()]);
+// console.log([...question]);
+// console.log(question.entries());
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
 //maps
 // const rest = new Map();
 
