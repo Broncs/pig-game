384741bfaebf75ee4cad066ee4a10e2082e62199
broncs <jhowfrombrazil@gmail.com>
2021-04-06 19:41:48 -0300

    HOF

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -59,3 +59,31 @@ const arrTesting = arr => {
 arrTesting(testArr);
 console.log(testArr);
 */
+
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// HOF
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('HIII');
+};
+
+document.body.addEventListener('click', high5);
+
+['jonas', 'martha', 'adam'].forEach(high5);
