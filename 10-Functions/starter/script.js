dfe9a80764f80d03db8925872352d00a99278f8d
broncs <jhowfrombrazil@gmail.com>
2021-04-07 09:59:09 -0300

    challenge about call and apply

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -59,7 +59,7 @@ const arrTesting = arr => {
 arrTesting(testArr);
 console.log(testArr);
 */
-
+/*
 const oneWord = function (str) {
   return str.replaceAll(' ', '').toLowerCase();
 };
@@ -87,3 +87,178 @@ const high5 = function () {
 document.body.addEventListener('click', high5);
 
 ['jonas', 'martha', 'adam'].forEach(high5);
+*/
+/*
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('steve');
+greet('hello')('jhon');
+
+const greet2 = greeting => name2 => console.log(`${greeting} ${name2}`);
+
+greet2('EAI')('JHONATAN');
+*/
+
+/*
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jhonatan mendes');
+lufthansa.book(635, 'john smith');
+console.log(lufthansa.bookings);
+
+const eurowings = {
+  airline: 'eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+//call methods
+const book = lufthansa.book;
+// book(23, 'Sarah williams'); don't work
+book.call(eurowings, 23, 'Sarah williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'swiss air line',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+
+//apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+// Bind method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jhonatan');
+
+// with event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+// const addTax = (rate, value) => value + value * rate;
+
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// console.log(addVAT(100));
+/*
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: Javascript', '1: Python', '2: rust', '3: C++'],
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const newOutput = `${this.question}\n${this.options.join(
+      '\n'
+    )}\n(Write option number)`;
+    const answerQuestion = Number(prompt(newOutput));
+    //
+
+    typeof answerQuestion === 'number' &&
+      answerQuestion < this.options.length &&
+      this.answers[answerQuestion]++;
+
+    this.displayResults('string');
+    this.displayResults();
+  },
+  displayResults(type = 'array') {
+    console.log(
+      type === 'string'
+        ? `Poll results are ${this.answers.join(', ')}`
+        : [this.answers]
+    );
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 8] }, 'string');
+poll.displayResults.call({ answers: [5, 2, 3] });
