9c9566753e4303d19f55d4b22344b3e4c5b5869b
broncs <jhowfrombrazil@gmail.com>
2021-04-12 12:58:05 -0300

    OOP constructor function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties .
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  this.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+};
+
+const jhonatan = new Person('Johnatan', 1995);
+console.log(jhonatan);
+const matilda = new Person('matilda', 2017);
+const jack = new Person('jack', 1975);
+
+console.log(matilda);
+console.log(jack);
+
+console.log(jhonatan instanceof Person);
