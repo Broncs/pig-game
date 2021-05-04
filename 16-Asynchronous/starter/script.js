bd243fe76ade4f29d4a920a3b92420c900597381
broncs <jhowfrombrazil@gmail.com>
2021-05-04 11:38:14 -0300

    combinator function promise.all

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -28,6 +28,14 @@ const renderCountry = function (data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
+const getJSON = function (url, errorMessage = 'Something went wrong') {
+  return fetch(url).then(res => {
+    if (!res.ok) {
+      throw new Error(`${errorMessage} (${res.status})`);
+    }
+    return res.json();
+  });
+};
 ///////////////////////////////////////
 /*
 const getCountryData = function (country) {
@@ -453,6 +461,7 @@ createImage('./img/img-1.jpg')
 //     return wait(1);
 //   });
 */
+/*
 const getPosition = () => {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -509,3 +518,30 @@ console.log('1: will get location');
     console.log('3: finished getting location');
   }
 })();
+*/
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+get3Countries('portugal', 'canada', 'brazil');
