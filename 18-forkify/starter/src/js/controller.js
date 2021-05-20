20eb49ef76a9b65cc925d401e5cee4e7b7aeb537
broncs <jhowfrombrazil@gmail.com>
2021-05-20 13:07:00 -0300

    Changing servings functionality

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -25,9 +25,9 @@ const controlRecipes = async () => {
 
     recipeView.renderSpinner();
 
-    // 2_ Rendering recipe
     await model.loadRecipe(id);
 
+    // 2_ Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
     recipeView.renderError();
@@ -65,8 +65,17 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
